5a9d8a18c8ebb7c3151cc4af8d6d499ca85f037e
Natan Dias <49736127+natandias@users.noreply.github.com>
2019-06-03 02:05:24 -0300

    Adicionado Função Listar e consertado carregamento dos dados

--- arvoreB/main.c
+++ arvoreB/main.c
@@ -24,168 +24,167 @@
   18. type		Numeric (integer values in range [1,7])
   */
 
-  struct animal {
-        char nome[20];
-        int cabelo;
-        int penas;
-        int ovos;
-        int leite;
-        int ave;
-        int aquatico;
-        int predador;
-        int dentado;
-        int espinha;
-        int respira;
-        int venenoso;
-        int barbatanas;
-        int pernas;
-        int rabo;
-        int domestico;
-        int catsize;
-        int tipo;
-  };
-  typedef struct animal Animal;
+struct animal
+{
+    char nome[20];
+    int cabelo;
+    int penas;
+    int ovos;
+    int leite;
+    int ave;
+    int aquatico;
+    int predador;
+    int dentado;
+    int espinha;
+    int respira;
+    int venenoso;
+    int barbatanas;
+    int pernas;
+    int rabo;
+    int domestico;
+    int catsize;
+    int tipo;
+};
+typedef struct animal Animal;
+
+void ListarAnimais(Animal *animal, int tam)
+{
+    int i=0;
+
+    for (i=0; i<tam; i++)
+    {
+
+        printf("%s ", animal[i].nome);
+        printf("%d ", animal[i].cabelo);
+        printf("%d ", animal[i].penas);
+        printf("%d ", animal[i].ovos);
+        printf("%d ", animal[i].leite);
+        printf("%d ", animal[i].ave);
+        printf("%d ", animal[i].aquatico);
+        printf("%d ", animal[i].predador);
+        printf("%d ", animal[i].dentado);
+        printf("%d ", animal[i].espinha);
+        printf("%d ", animal[i].respira);
+        printf("%d ", animal[i].venenoso);
+        printf("%d ", animal[i].barbatanas);
+        printf("%d ", animal[i].pernas);
+        printf("%d ", animal[i].rabo);
+        printf("%d ", animal[i].domestico);
+        printf("%d ", animal[i].catsize);
+        printf("%d ", animal[i].tipo);
+    }
+}
 
 int main()
 {
     printf("===================================\n");
-    printf("===========ZOO DATABASE============\n");
+    printf("===========ZOO DATABASE============\n\n");
 
     FILE* ptr;
-   if (fopen("zoo.data", "r")==NULL) {
+    if (fopen("zoo.data", "rb")==NULL)
+    {
         printf("Nao foi possivel abrir o arquivo\n\n");
         return 1;
-   }
+    }
 
-   ptr = fopen("zoo.data", "r");
+    ptr = fopen("zoo.data", "rb");
 
     int i=0, Chave;
     BTree* raiz, *Buscado;
     raiz = NovaBTree();
 
-Animal NovoAnimal[101];
+    Animal NovoAnimal[101];
 
- while(!feof(ptr)){
-
-    fscanf(ptr, "%[^,]s", &NovoAnimal[i].nome);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].cabelo);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]",&NovoAnimal[i].penas);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].ovos);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].leite);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].ave);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].aquatico);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].predador);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].dentado);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].espinha);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].respira);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].venenoso);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].barbatanas);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].pernas);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].rabo);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].domestico);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].catsize);
-
-    fseek(ptr, 1, SEEK_CUR);
-    fscanf(ptr, "%d[^,]", &NovoAnimal[i].tipo);
-
-    fseek(ptr, 1, SEEK_CUR);
-
-    printf("%s\t", NovoAnimal[i].nome);
-     printf("%d ", NovoAnimal[i].cabelo);
-      printf("%d ", NovoAnimal[i].penas);
-       printf("%d ", NovoAnimal[i].ovos);
-        printf("%d ", NovoAnimal[i].leite);
-         printf("%d ", NovoAnimal[i].ave);
-          printf("%d ", NovoAnimal[i].aquatico);
-           printf("%d ", NovoAnimal[i].predador);
-            printf("%d ", NovoAnimal[i].dentado);
-             printf("%d ", NovoAnimal[i].espinha);
-              printf("%d ", NovoAnimal[i].respira);
-               printf("%d ", NovoAnimal[i].venenoso);
-                printf("%d ", NovoAnimal[i].barbatanas);
-                printf("%d ", NovoAnimal[i].pernas);
-                printf("%d ", NovoAnimal[i].rabo);
-                printf("%d ", NovoAnimal[i].domestico);
-                printf("%d ", NovoAnimal[i].catsize);
-                printf("%d ", NovoAnimal[i].tipo);
-
-i++;
-  }
-
-
-
-
-//}
-
-   /* for (i=0; i<feof(ptr); i++){
-       fscanf(ptr, "%[^,]s", &nome);
-       Animal *nome[i];
-       fscanf(ptr, "%[^,]d", &nome[i]->nome);
-    }*/
-
-    /*for(i = 0; i < MAXITENS; i++)
+    while(!feof(ptr))
     {
-        Chave = (rand() % (MAXITENS * 5));
-        if(BuscaBTree(raiz, Chave) == NULL)
-            raiz = InsereBTree(raiz, Chave);
-    }*/
-/*
 
-   raiz = InsereBTree(raiz, 10);
+        fscanf(ptr, "%[^,]s", &NovoAnimal[i].nome);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].cabelo);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d",&NovoAnimal[i].penas);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].ovos);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].leite);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].ave);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].aquatico);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].predador);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].dentado);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].espinha);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].respira);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].venenoso);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].barbatanas);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].pernas);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].rabo);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].domestico);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].catsize);
+
+        fseek(ptr, 1, SEEK_CUR);
+        fscanf(ptr, "%d", &NovoAnimal[i].tipo);
+
+        if (feof(ptr))
+            break;
+        i++;
+    };
+    ListarAnimais(NovoAnimal, i);
+
+    printf("\n\n fdsafasdf");
 
 
 
-    Imprime(raiz);
 
-    printf("Altura = %d\n\n", Altura(raiz));
-    printf("MenorChave = %d\n\n", MenorChave(raiz));
+    /*
 
-    printf("Chave para buscar: ");
-    scanf("%d", &Chave);
-    Buscado = BuscaBTree(raiz, Chave);
-    printf("\n");
+            Imprime(raiz);
+       printf(" %d", i);
+            printf("Altura = %d\n\n", Altura(raiz));
+            printf("MenorChave = %d\n\n", MenorChave(raiz));
 
-    if(Buscado == NULL)
-    {
-        printf("Chave nao encontrada!!\n");
-    }
-    else
-    {
-        printf("Chave Encontrada!!\n");
-        printf("Em %d\n", Buscado->Chaves[0]);
-    }
-*/
+            printf("Chave para buscar: ");
+            scanf("%d", &Chave);
+            Buscado = BuscaBTree(raiz, Chave);
+            printf("\n");
+
+            if(Buscado == NULL)
+            {
+                printf("Chave nao encontrada!!\n");
+            }
+            else
+            {
+                printf("Chave Encontrada!!\n");
+                printf("Em %d\n", Buscado->Chaves[0]);
+            }
+
+    */
+
     return 0;
-   }
+}
